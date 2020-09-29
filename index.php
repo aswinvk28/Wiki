076e97ef45e48e3a1d052fa5eaf9035464cb97a5
Aswin Vijayakumar <aswinvk28@gmail.com>
2020-09-29 23:57:12 +0100

    create index.php
    index.php added

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylorotwell@gmail.com>
+ */
+
+require_once __DIR__.'/config.php';
+
+require_once __DIR__.'/index.php';
